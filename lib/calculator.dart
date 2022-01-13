3baab06eaed767a8ae1ea0802e91f31fb299d598
Josh <mayankgouravcool1@gmail.com>
2022-01-14 01:24:49 +0530

    basic UI done

---
+++ lib/calculator.dart
@@ -0,0 +1,387 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double height = mediaQueryData.size.height;
+    double width = mediaQueryData.size.width;
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: (height / 3) - 25,
+                width: width,
+                child: Container(
+                    // color: Colors.black,
+                    ),
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: width,
+                height: (2 * height) / 15,
+                child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "C",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.brown, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Icon(Icons.backspace_outlined,
+                                color: Colors.white),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "%",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "/",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width,
+                height: (2 * height) / 15,
+                child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "7",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "8",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "9",
+                              style: TextStyle(fontSize: 30,color: Colors.white),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "*",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width,
+                height: (2 * height) / 15,
+                child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "4",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "5",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "6",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "-",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width,
+                height: (2 * height) / 15,
+                child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "1",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "2",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "3",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "+",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width,
+                height: (2 * height) / 15,
+                child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "+/-",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "0",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              ".",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "=",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(20),
+                              primary: Colors.black12, // <-- Button color
+                              onPrimary: Colors.white, // <-- Splash color
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:state_management/calculator.dart';
 
 void main(){
-  runApp(const App());
-}
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: Colors.black
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          // children: [
-          //   SizedBox(
-          //     width: MediaQuery.of(context).size.width,
-          //     height: 400,
-          //   ),
-          //   Row(),
-          //   Row(),
-          //   Row(),
-          //   Row(),
-          //   Row()
-          // ],
-        ),
-      ),
-      color: Colors.black,
-    );
-  }
+  runApp(const MaterialApp(
+    home: Calculator(),
+  ));
 }
