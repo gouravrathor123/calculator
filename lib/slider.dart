ee9b0c242991e53483fdba0c3c52e313962895a1
Josh <mayankgouravcool1@gmail.com>
2022-01-13 11:48:49 +0530

    first commit

---
+++ lib/slider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MySlider extends StatefulWidget {
+  const MySlider({Key? key}) : super(key: key);
+
+  @override
+  _MySliderState createState() => _MySliderState();
+}
+
+class _MySliderState extends State<MySlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
